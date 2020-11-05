318c70edea0292b5ecec3df31dbb48b16979bb8f
hieuls369 <hieuls369@gmail.com>
2020-11-05 23:17:56 +0700

    tien duoc hieu update

---
+++ app/src/main/java/com/example/ohsheet/view/DetailActivity.java
@@ -0,0 +1,27 @@
+package com.example.ohsheet.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.ohsheet.R;
+
+public class DetailActivity extends AppCompatActivity {
+    private TextView txtTitle;
+    private TextView txtWriter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        txtTitle = findViewById(R.id.txtTitle);
+        txtWriter = findViewById(R.id.txtWriter);
+        Intent intent = new Intent();
+        String title = intent.getStringExtra("title");
+        String writer = intent.getStringExtra("writer");
+        txtTitle.setText(title);
+        txtWriter.setText(writer);
+    }
+}
--- app/src/main/java/com/example/ohsheet/view/ListAllSheet.java
+++ app/src/main/java/com/example/ohsheet/view/ListAllSheet.java
@@ -5,28 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.example.ohsheet.R;
 import com.example.ohsheet.adapter.SongAdapter;
 import com.example.ohsheet.entity.Song;
 
 import java.util.ArrayList;
 import java.util.List;
 
-//public class ListAllSheet extends AppCompatActivity {
-//    private ListView listView;
-//    private List<Song> list;
-//    private SongAdapter songAdapter;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_list_all_sheet);
-//        listView = findViewById(R.id.listView);
-//        list = new ArrayList<>();
+public class ListAllSheet extends AppCompatActivity {
+    private ListView listView;
+    private List<Song> list;
+    private SongAdapter songAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_all_sheet);
+        listView = findViewById(R.id.listView);
+        list = new ArrayList<>();
 //        list.add(new Song(1,"Hoa nở không màu","Hoài Lâm"));
 //        list.add(new Song(2,"Nàng thơ","Hoàng Dũng"));
 //        list.add(new Song(3,"Missing You","G-Dragon"));
-//        songAdapter = new SongAdapter(this,R.layout.customlayout,list);
-//        listView.setAdapter(songAdapter);
-//
-//
-//    }
-//}
+        songAdapter = new SongAdapter(this,R.layout.customlayout,list);
+        listView.setAdapter(songAdapter);
+
+
+    }
+}
